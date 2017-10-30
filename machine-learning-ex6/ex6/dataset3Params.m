b7edb4a3b15a1353627e938123ace13f5cae3bc6
Onyeka Igabari <oigabari@openmobileww.com>
2017-10-30 16:12:19 -0400

    Learn more about how to use an SVM
    - train SVM models based on guesses for C and sigma to
    determine the best C and sigma values for the cross evaluation set

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,8 +22,38 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+x1 = [5 2 4];
+x2 = [0 1 -1];
 
+params = [0.01 0.03 0.1 0.3 1 3 10 30];
+p_size = size(params, 2);
+min_error = 1000;
 
+% train SVM models based on guesses for C and sigma to
+% determine the best C and sigma values for the cross evaluation set
+%fprintf('Xval:[%dx%d], yval:[%dx%d], p_size: %d\n', size(Xval), size(yval), p_size);
+%for i = 1:p_size,
+%    for j = 1: p_size,
+%        C_tmp = params(i);
+%        sigma_tmp = params(j);
+%        model = svmTrain(X,y, C_tmp, @(x1, x2) gaussianKernel(x1, x2, sigma_tmp));
+%        visualizeBoundaryLinear(X, y, model);
+%        [predictions] = svmPredict(model, Xval);
+%        err = mean(double(predictions ~= yval));
+%        printf('err: %f, C: %f, sigma: %f\n', err, C_tmp, sigma_tmp);
+%        if min_error > err,
+%            min_error = err;
+%            C = C_tmp;
+%            sigma = sigma_tmp;
+%            printf('!!!!! Choice Value C: %f, sigma: %f\n', C_tmp, sigma_tmp);
+%        end
+%    end
+%end
+%fprintf('C: %f sigma: %f\n', C, sigma);
+
+% selected values based on lowest error returned
+C = 1;
+sigma = 0.1;
 
 
 
